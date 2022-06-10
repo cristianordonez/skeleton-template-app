d346e61b36f94ddfe4aec884bfce6d7f62d76d4f
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-06-10 15:28:55 -0400

    Added initital config files

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 1128;
+const app = express();
+const compression = require('compression');
+
+//MIDDLEWARE
+app.use(compression());
+
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+//ROUTES
+app.get('/', (req, res) => {
+   res.status(200).json({
+      status: 'success',
+      data: {
+         name: 'template',
+         version: '1.0.0',
+      },
+   });
+});
+
+//START SERVER
+app.listen(port, () => {
+   console.log(`App listening on port ${port}`);
+});
