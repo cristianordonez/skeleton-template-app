50393f6804d2f2939ee576f16eb239ae6a68ae00
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-08-16 20:42:50 -0400

    edited initial jest test sample file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import { App } from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
