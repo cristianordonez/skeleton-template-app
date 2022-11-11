fcf8b5f7e387316807aa55ed2fd4f49c0bb7c2db
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-11-11 15:39:09 -0500

    added express body parser

--- server/app.ts
+++ server/app.ts
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(compression());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTES
